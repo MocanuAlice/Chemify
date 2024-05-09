1316811d07b75a5cbee5fe3c3b975d74b2858a4e
MocanuAlice <alicemocanu06@gmail.com>
2024-05-09 09:10:52 +0300

    Add project files.

---
+++ Chemify/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Chemify
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void signUpBtn_Click(object sender, RoutedEventArgs e)
+        {
+            signUp w1 = new signUp();
+            //Visibility = Visibility.Hidden;
+            w1.Show();
+        }
+
+        private void signInBtn_Click(object sender, RoutedEventArgs e)
+        {
+            signIn w2 = new signIn();
+            //Visibility = Visibility.Hidden;
+            w2.Show();
+        }
+    }
+}
---
+++ Chemify/signIn.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Chemify
+{
+    /// <summary>
+    /// Interaction logic for signIn.xaml
+    /// </summary>
+    public partial class signIn : Window
+    {
+        public signIn()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            //Visibility = Visibility.Visible;
+        }
+    }
+}
