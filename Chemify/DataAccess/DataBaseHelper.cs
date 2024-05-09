158d43700b8d3b32dd320e898a048057306579b2
IonescuOanaAlexandra <oana-alexandra.ionescu@cncn.ro>
2024-05-09 19:34:48 +0300

    ma spanzur

---
+++ Chemify/DataAccess/DataBaseHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.IO;
+using System.Data;
+
+
+namespace Chemify.DataAccess
+{
+    public class DataBaseHelper
+    {
+        private static readonly string _connectionString = SQLDataAccess.GetConnectionString();
+        public static void Initialisation()
+        {
+            using (SqlConnection con = new SqlConnection(_connectionString))
+            {
+                con.Open();
+            }
+        }
+
+    }
+}
---
+++ Chemify/DataAccess/SQLDataAccess.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace Chemify.DataAccess
+{
+    class SQLDataAccess
+    {
+        public static string GetConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+        }
+    }
+}
